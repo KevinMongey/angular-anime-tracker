85dd0d92d897f5199253567249916f63c767e519
Kevin Mongey <kevin.r.mongey@gmail.com>
2026-08-12 09:58:38 +0100

    Added info to cards

--- src/app/anime-row/anime-row.component.ts
+++ src/app/anime-row/anime-row.component.ts
@@ -1,15 +1,23 @@
 import { Component, input, Input } from '@angular/core';
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonChip, IonIcon } from '@ionic/angular/standalone';
+import { starSharp } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-anime-row',
   templateUrl: './anime-row.component.html',
   styleUrls: ['./anime-row.component.scss'],
   standalone: true,
-  imports: [IonCard, IonCardContent, IonCardHeader, IonCardTitle],
+  imports: [IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonChip, IonIcon],
 })
 export class AnimeRowComponent {
 
   @Input() title = '';
   @Input() anime: any[] = [];
+
+  constructor() {
+    addIcons({
+      starSharp
+    });
+  }
 }
